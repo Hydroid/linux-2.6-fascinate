303b52e12d4b16ee6c09c173776d48f9d6a5b3eb
imnuts <mdietz@alum.bucknell.edu>
2011-01-20 02:55:06 -0500

    Replace novoodoo with aosp

--- build_kernels.sh
+++ build_kernels.sh
@@ -10,17 +10,19 @@ echo -e "***                                         ***"
 echo -e "***********************************************"
 cp -f arch/arm/plat-s5pc11x/max8998_consumer.sv arch/arm/plat-s5pc11x/max8998_consumer.c
 
-# Build DJ05/DL09 Kernels
-./build_kernel.sh A
+# Build DJ05/DL09 Kernel
+./build_voodoo.sh
 
-# Build Foyo Kernels
+# Build AOSP Kernel
+./build_aosp_voodoo.sh
+
+# Build Froyo Kernel
 ./build_froyo_voodoo.sh
 
 # Copy kernels to Dropbox
 mv "$DATE"_imnuts_voodoo5.zip ../../Dropbox/Public/Kernels/"$DATE"_imnuts_sv_voodoo5.zip
-mv "$DATE"_imnuts_novoodoo.zip ../../Dropbox/Public/Kernels/"$DATE"_imnuts_sv_novoodoo.zip
+mv "$DATE"_imnuts_aosp_voodoo5.zip ../../Dropbox/Public/Kernels/"$DATE"_imnuts_aosp_sv_voodoo5.zip
 mv "$DATE"_imnuts_froyo_voodoo5.zip ../../Dropbox/Public/Kernels/"$DATE"_imnuts_froyo_sv_voodoo5.zip
-#mv "$DATE"_imnuts_aosp_novoodoo.zip ../../Dropbox/Public/Kernels/"$DATE"_imnuts_aosp_sv_novoodoo.zip
 
 # Build -25mV Kernels
 echo -e "***********************************************"
@@ -30,17 +32,19 @@ echo -e "***                                         ***"
 echo -e "***********************************************"
 cp -f arch/arm/plat-s5pc11x/max8998_consumer.25 arch/arm/plat-s5pc11x/max8998_consumer.c
 
-# Build DJ05/DL09 Kernels
-./build_kernel.sh A
+# Build DJ05/DL09 Kernel
+./build_voodoo.sh
 
-# Build Froyo Kernels
+# Build AOSP Kernel
+./build_aosp_voodoo.sh
+
+# Build Froyo Kernel
 ./build_froyo_voodoo.sh
 
 # Copy kernels to Dropbox
 mv "$DATE"_imnuts_voodoo5.zip ../../Dropbox/Public/Kernels/"$DATE"_imnuts_25_voodoo5.zip
-mv "$DATE"_imnuts_novoodoo.zip ../../Dropbox/Public/Kernels/"$DATE"_imnuts_25_novoodoo.zip
+mv "$DATE"_imnuts_aosp_voodoo5.zip ../../Dropbox/Public/Kernels/"$DATE"_imnuts_aosp_25_voodoo5.zip
 mv "$DATE"_imnuts_froyo_voodoo5.zip ../../Dropbox/Public/Kernels/"$DATE"_imnuts_froyo_25_voodoo5.zip
-#mv "$DATE"_imnuts_aosp_novoodoo.zip ../../Dropbox/Public/Kernels/"$DATE"_imnuts_aosp_25_novoodoo.zip
 
 # Build -50mV Kernels
 echo -e "***********************************************"
@@ -50,17 +54,19 @@ echo -e "***                                         ***"
 echo -e "***********************************************"
 cp -f arch/arm/plat-s5pc11x/max8998_consumer.50 arch/arm/plat-s5pc11x/max8998_consumer.c
 
-# Build DJ05/DL09 Kernels
-./build_kernel.sh A
+# Build DJ05/DL09 Kernel
+./build_voodoo.sh
 
-# Build Froyo Kernels
+# Build AOSP Kernel
+./build_aosp_voodoo.sh
+
+# Build Froyo Kernel
 ./build_froyo_voodoo.sh
 
 # Copy kernels to Dropbox
 mv "$DATE"_imnuts_voodoo5.zip ../../Dropbox/Public/Kernels/"$DATE"_imnuts_50_voodoo5.zip
-mv "$DATE"_imnuts_novoodoo.zip ../../Dropbox/Public/Kernels/"$DATE"_imnuts_50_novoodoo.zip
+mv "$DATE"_imnuts_aosp_voodoo5.zip ../../Dropbox/Public/Kernels/"$DATE"_imnuts_aosp_50_voodoo5.zip
 mv "$DATE"_imnuts_froyo_voodoo5.zip ../../Dropbox/Public/Kernels/"$DATE"_imnuts_froyo_50_voodoo5.zip
-#mv "$DATE"_imnuts_aosp_novoodoo.zip ../../Dropbox/Public/Kernels/"$DATE"_imnuts_aosp_50_novoodoo.zip
 
 # Build -75mV Kernels
 echo -e "***********************************************"
@@ -70,17 +76,19 @@ echo -e "***                                         ***"
 echo -e "***********************************************"
 cp -f arch/arm/plat-s5pc11x/max8998_consumer.75 arch/arm/plat-s5pc11x/max8998_consumer.c
 
-# Build DJ05/DL09 Kernels
-./build_kernel.sh A
+# Build DJ05/DL09 Kernel
+./build_voodoo.sh
 
-# Build Froyo Kernels
+# Build AOSP Kernel
+./build_aosp_voodoo.sh
+
+# Build Froyo Kernel
 ./build_froyo_voodoo.sh
 
 # Copy kernels to Dropbox
 mv "$DATE"_imnuts_voodoo5.zip ../../Dropbox/Public/Kernels/"$DATE"_imnuts_75_voodoo5.zip
-mv "$DATE"_imnuts_novoodoo.zip ../../Dropbox/Public/Kernels/"$DATE"_imnuts_75_novoodoo.zip
+mv "$DATE"_imnuts_aosp_voodoo5.zip ../../Dropbox/Public/Kernels/"$DATE"_imnuts_aosp_75_voodoo5.zip
 mv "$DATE"_imnuts_froyo_voodoo5.zip ../../Dropbox/Public/Kernels/"$DATE"_imnuts_froyo_75_voodoo5.zip
-#mv "$DATE"_imnuts_aosp_novoodoo.zip ../../Dropbox/Public/Kernels/"$DATE"_imnuts_aosp_75_novoodoo.zip
 
 # Build -100mV Kernels
 echo -e "***********************************************"
@@ -90,17 +98,19 @@ echo -e "***                                         ***"
 echo -e "***********************************************"
 cp -f arch/arm/plat-s5pc11x/max8998_consumer.100 arch/arm/plat-s5pc11x/max8998_consumer.c
 
-# Build DJ05/DL09 Kernels
-./build_kernel.sh A
+# Build DJ05/DL09 Kernel
+./build_voodoo.sh
 
-# Build Froyo Kernels
+# Build AOSP Kernel
+./build_aosp_voodoo.sh
+
+# Build Froyo Kernel
 ./build_froyo_voodoo.sh
 
 # Copy kernels to Dropbox
 mv "$DATE"_imnuts_voodoo5.zip ../../Dropbox/Public/Kernels/"$DATE"_imnuts_100_voodoo5.zip
-mv "$DATE"_imnuts_novoodoo.zip ../../Dropbox/Public/Kernels/"$DATE"_imnuts_100_novoodoo.zip
+mv "$DATE"_imnuts_aosp_voodoo5.zip ../../Dropbox/Public/Kernels/"$DATE"_imnuts_aosp_100_voodoo5.zip
 mv "$DATE"_imnuts_froyo_voodoo5.zip ../../Dropbox/Public/Kernels/"$DATE"_imnuts_froyo_100_voodoo5.zip
-#mv "$DATE"_imnuts_aosp_novoodoo.zip ../../Dropbox/Public/Kernels/"$DATE"_imnuts_aosp_100_novoodoo.zip
 
 # Build -125mV Kernels
 echo -e "***********************************************"
@@ -110,17 +120,19 @@ echo -e "***                                         ***"
 echo -e "***********************************************"
 cp -f arch/arm/plat-s5pc11x/max8998_consumer.125 arch/arm/plat-s5pc11x/max8998_consumer.c
 
-# Build DJ05/DL09 Kernels
-./build_kernel.sh A
+# Build DJ05/DL09 Kernel
+./build_voodoo.sh
 
-# Build Froyo Kernels
+# Build AOSP Kernel
+./build_aosp_voodoo.sh
+
+# Build Froyo Kernel
 ./build_froyo_voodoo.sh
 
 # Copy kernels to Dropbox
 mv "$DATE"_imnuts_voodoo5.zip ../../Dropbox/Public/Kernels/"$DATE"_imnuts_125_voodoo5.zip
-mv "$DATE"_imnuts_novoodoo.zip ../../Dropbox/Public/Kernels/"$DATE"_imnuts_125_novoodoo.zip
+mv "$DATE"_imnuts_aosp_voodoo5.zip ../../Dropbox/Public/Kernels/"$DATE"_imnuts_aosp_125_voodoo5.zip
 mv "$DATE"_imnuts_froyo_voodoo5.zip ../../Dropbox/Public/Kernels/"$DATE"_imnuts_froyo_125_voodoo5.zip
-#mv "$DATE"_imnuts_aosp_novoodoo.zip ../../Dropbox/Public/Kernels/"$DATE"_imnuts_aosp_125_novoodoo.zip
 
 # Update url_list.txt with links to kernels
 echo -e "***********************************************"
@@ -130,26 +142,20 @@ echo -e "***                                         ***"
 echo -e "***********************************************"
 
 echo http://dl.dropbox.com/u/17111376/Kernels/"$DATE"_imnuts_sv_voodoo5.zip >> ../../Dropbox/Public/Kernels/url_list.txt
-echo http://dl.dropbox.com/u/17111376/Kernels/"$DATE"_imnuts_sv_novoodoo.zip >> ../../Dropbox/Public/Kernels/url_list.txt
 echo http://dl.dropbox.com/u/17111376/Kernels/"$DATE"_imnuts_25_voodoo5.zip >> ../../Dropbox/Public/Kernels/url_list.txt
-echo http://dl.dropbox.com/u/17111376/Kernels/"$DATE"_imnuts_25_novoodoo.zip >> ../../Dropbox/Public/Kernels/url_list.txt
 echo http://dl.dropbox.com/u/17111376/Kernels/"$DATE"_imnuts_50_voodoo5.zip >> ../../Dropbox/Public/Kernels/url_list.txt
-echo http://dl.dropbox.com/u/17111376/Kernels/"$DATE"_imnuts_50_novoodoo.zip >> ../../Dropbox/Public/Kernels/url_list.txt
 echo http://dl.dropbox.com/u/17111376/Kernels/"$DATE"_imnuts_75_voodoo5.zip >> ../../Dropbox/Public/Kernels/url_list.txt
-echo http://dl.dropbox.com/u/17111376/Kernels/"$DATE"_imnuts_75_novoodoo.zip >> ../../Dropbox/Public/Kernels/url_list.txt
 echo http://dl.dropbox.com/u/17111376/Kernels/"$DATE"_imnuts_100_voodoo5.zip >> ../../Dropbox/Public/Kernels/url_list.txt
-echo http://dl.dropbox.com/u/17111376/Kernels/"$DATE"_imnuts_100_novoodoo.zip >> ../../Dropbox/Public/Kernels/url_list.txt
 echo http://dl.dropbox.com/u/17111376/Kernels/"$DATE"_imnuts_125_voodoo5.zip >> ../../Dropbox/Public/Kernels/url_list.txt
-echo http://dl.dropbox.com/u/17111376/Kernels/"$DATE"_imnuts_125_novoodoo.zip >> ../../Dropbox/Public/Kernels/url_list.txt
+echo http://dl.dropbox.com/u/17111376/Kernels/"$DATE"_imnuts_aosp_sv_voodoo5.zip >> ../../Dropbox/Public/Kernels/url_list.txt
+echo http://dl.dropbox.com/u/17111376/Kernels/"$DATE"_imnuts_aosp_25_voodoo5.zip >> ../../Dropbox/Public/Kernels/url_list.txt
+echo http://dl.dropbox.com/u/17111376/Kernels/"$DATE"_imnuts_aosp_50_voodoo5.zip >> ../../Dropbox/Public/Kernels/url_list.txt
+echo http://dl.dropbox.com/u/17111376/Kernels/"$DATE"_imnuts_aosp_75_voodoo5.zip >> ../../Dropbox/Public/Kernels/url_list.txt
+echo http://dl.dropbox.com/u/17111376/Kernels/"$DATE"_imnuts_aosp_100_voodoo5.zip >> ../../Dropbox/Public/Kernels/url_list.txt
+echo http://dl.dropbox.com/u/17111376/Kernels/"$DATE"_imnuts_aosp_125_voodoo5.zip >> ../../Dropbox/Public/Kernels/url_list.txt
 echo http://dl.dropbox.com/u/17111376/Kernels/"$DATE"_imnuts_froyo_sv_voodoo5.zip >> ../../Dropbox/Public/Kernels/url_list.txt
-#echo http://dl.dropbox.com/u/17111376/Kernels/"$DATE"_imnuts_aosp_sv_novoodoo.zip >> ../../Dropbox/Public/Kernels/url_list.txt
 echo http://dl.dropbox.com/u/17111376/Kernels/"$DATE"_imnuts_froyo_25_voodoo5.zip >> ../../Dropbox/Public/Kernels/url_list.txt
-#echo http://dl.dropbox.com/u/17111376/Kernels/"$DATE"_imnuts_aosp_25_novoodoo.zip >> ../../Dropbox/Public/Kernels/url_list.txt
 echo http://dl.dropbox.com/u/17111376/Kernels/"$DATE"_imnuts_froyo_50_voodoo5.zip >> ../../Dropbox/Public/Kernels/url_list.txt
-#echo http://dl.dropbox.com/u/17111376/Kernels/"$DATE"_imnuts_aosp_50_novoodoo.zip >> ../../Dropbox/Public/Kernels/url_list.txt
 echo http://dl.dropbox.com/u/17111376/Kernels/"$DATE"_imnuts_froyo_75_voodoo5.zip >> ../../Dropbox/Public/Kernels/url_list.txt
-#echo http://dl.dropbox.com/u/17111376/Kernels/"$DATE"_imnuts_aosp_75_novoodoo.zip >> ../../Dropbox/Public/Kernels/url_list.txt
 echo http://dl.dropbox.com/u/17111376/Kernels/"$DATE"_imnuts_froyo_100_voodoo5.zip >> ../../Dropbox/Public/Kernels/url_list.txt
-#echo http://dl.dropbox.com/u/17111376/Kernels/"$DATE"_imnuts_aosp_100_novoodoo.zip >> ../../Dropbox/Public/Kernels/url_list.txt
 echo http://dl.dropbox.com/u/17111376/Kernels/"$DATE"_imnuts_froyo_125_voodoo5.zip >> ../../Dropbox/Public/Kernels/url_list.txt
-#echo http://dl.dropbox.com/u/17111376/Kernels/"$DATE"_imnuts_aosp_125_novoodoo.zip >> ../../Dropbox/Public/Kernels/url_list.txt
