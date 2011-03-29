9c9702a2fdeb525c69d12837addd05cc4314d81e
imnuts <mdietz@alum.bucknell.edu>
2011-03-29 18:01:48 -0400

    Build scripts

---
+++ build_kernel.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# setup
+WORK=`pwd`
+DATE=$(date +%m%d)
+
+# execution!
+cd ..
+
+# check for voodoo/non-voodoo
+if [ "$1" != "V" ]; then
+	CONFIG="novoodoo"
+else
+	CONFIG="voodoo"
+fi
+
+# build the kernel
+cd $WORK
+echo "***** Building : $CONFIG *****"
+make clean mrproper > /dev/null 2>&1
+rm -f update_"$CONFIG"/*.zip update_"$CONFIG"/kernel_update/zImage
+
+make ARCH=arm imnuts_\"$CONFIG\"_defconfig
+make -j6 CROSS_COMPILE=/opt/toolchains/arm-2009q3/bin/arm-none-linux-gnueabi- ARCH=arm HOSTCFLAGS="-g -O3" 1>"$WORK"/stdlog.txt 2>"$WORK"/errlog.txt
+
+cp arch/arm/boot/zImage update_"$CONFIG"/kernel_update/zImage
+cd update_"$CONFIG"
+zip -r -q kernel_update.zip .
+mv kernel_update.zip ../"$DATE"_imnuts_"$CONFIG".zip
+cd ..
+#cp "$DATE"_imnuts_"$CONFIG".zip /home/imnuts/Dropbox/Public/Kernels/"$DATE"_imnuts_"$CONFIG".zip
+echo -e "***** Successfully compiled: $CONFIG *****\n"
+
+if [ "$1" == "A" ]; then ./build_kernel.sh V; fi
---
+++ build_kernels.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+DATE=$(date +%m%d)
+
+# Build Stock Voltage Kernels
+echo -e "***********************************************"
+echo -e "***                                         ***"
+echo -e "***      Building Stock Voltage Kernel      ***"
+echo -e "***                                         ***"
+echo -e "***********************************************"
+cp -f arch/arm/mach-s5pv210/atlas/max8998_consumer.0 arch/arm/mach-s5pv210/atlas/max8998_consumer.c
+
+# Build Kernels
+./build_kernel.sh A
+
+# Move kernels to Dropbox
+mv "$DATE"_imnuts_voodoo.zip ../../../Dropbox/Public/Kernels/"$DATE"_imnuts_sv_voodoo.zip
+mv "$DATE"_imnuts_novoodoo.zip ../../../Dropbox/Public/Kernels/"$DATE"_imnuts_sv_novoodoo.zip
+
+# Build -50mV Kernels
+echo -e "***********************************************"
+echo -e "***                                         ***"
+echo -e "***     Building -50mV Undervolt Kernel     ***"
+echo -e "***                                         ***"
+echo -e "***********************************************"
+cp -f arch/arm/mach-s5pv210/atlas/max8998_consumer.50 arch/arm/mach-s5pv210/atlas/max8998_consumer.c
+
+# Build Kernels
+./build_kernel.sh A
+
+# Move kernels to Dropbox
+mv "$DATE"_imnuts_voodoo.zip ../../../Dropbox/Public/Kernels/"$DATE"_imnuts_50_voodoo.zip
+mv "$DATE"_imnuts_novoodoo.zip ../../../Dropbox/Public/Kernels/"$DATE"_imnuts_50_novoodoo.zip
+
+# Build -100mV Kernels
+echo -e "***********************************************"
+echo -e "***                                         ***"
+echo -e "***    Building -100mV Undervolt Kernel     ***"
+echo -e "***                                         ***"
+echo -e "***********************************************"
+cp -f arch/arm/mach-s5pv210/atlas/max8998_consumer.100 arch/arm/mach-s5pv210/atlas/max8998_consumer.c
+
+# Build Kernels
+./build_kernel.sh A
+
+# Move kernels to Dropbox
+mv "$DATE"_imnuts_voodoo.zip ../../../Dropbox/Public/Kernels/"$DATE"_imnuts_100_voodoo.zip
+mv "$DATE"_imnuts_novoodoo.zip ../../../Dropbox/Public/Kernels/"$DATE"_imnuts_100_novoodoo.zip
+
+# Update url_list.txt with links to kernels
+echo -e "***********************************************"
+echo -e "***                                         ***"
+echo -e "***         Updating url_list.txt           ***"
+echo -e "***                                         ***"
+echo -e "***********************************************"
+
+echo http://dl.dropbox.com/u/17111376/Kernels/"$DATE"_imnuts_sv_voodoo.zip >> ../../../Dropbox/Public/Kernels/url_list.txt
+echo http://dl.dropbox.com/u/17111376/Kernels/"$DATE"_imnuts_50_voodoo.zip >> ../../../Dropbox/Public/Kernels/url_list.txt
+echo http://dl.dropbox.com/u/17111376/Kernels/"$DATE"_imnuts_100_voodoo.zip >> ../../../Dropbox/Public/Kernels/url_list.txt
+echo http://dl.dropbox.com/u/17111376/Kernels/"$DATE"_imnuts_sv_novoodoo.zip >> ../../../Dropbox/Public/Kernels/url_list.txt
+echo http://dl.dropbox.com/u/17111376/Kernels/"$DATE"_imnuts_50_novoodoo.zip >> ../../../Dropbox/Public/Kernels/url_list.txt
+echo http://dl.dropbox.com/u/17111376/Kernels/"$DATE"_imnuts_100_novoodoo.zip >> ../../../Dropbox/Public/Kernels/url_list.txt
