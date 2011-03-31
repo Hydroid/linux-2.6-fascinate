9320c38777e7564546074817d13f7c3da499f119
imnuts <mdietz@alum.bucknell.edu>
2011-03-31 00:39:54 -0400

    Quit if there is a build error

--- build_kernel.sh
+++ build_kernel.sh
@@ -20,8 +20,15 @@ echo "***** Building : $CONFIG *****"
 make clean mrproper > /dev/null 2>&1
 rm -f update_"$CONFIG"/*.zip update_"$CONFIG"/kernel_update/zImage
 
-make ARCH=arm imnuts_\"$CONFIG\"_defconfig
-make -j6 CROSS_COMPILE=/opt/toolchains/arm-2009q3/bin/arm-none-linux-gnueabi- ARCH=arm HOSTCFLAGS="-g -O3" 1>"$WORK"/stdlog.txt 2>"$WORK"/errlog.txt
+make ARCH=arm imnuts_"$CONFIG"_defconfig 1>/dev/null 2>"$WORK"/errlog.txt
+make -j6 CROSS_COMPILE=/opt/toolchains/arm-2009q3/bin/arm-none-linux-gnueabi- ARCH=arm HOSTCFLAGS="-g -O3" 1>"$WORK"/stdlog.txt 2>>"$WORK"/errlog.txt
+if [ $? != 0 ]; then
+		echo -e "FAIL!\n"
+		exit 1
+	else
+		echo -e "Success!\n"
+		rm -f "$WORK"/*log.txt
+fi
 
 cp arch/arm/boot/zImage update_"$CONFIG"/kernel_update/zImage
 cd update_"$CONFIG"
