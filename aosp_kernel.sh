6395e9448015ee5cca9ad423e9758402980727f5
Mark Dietz <mdietz@alum.bucknell.edu>
2011-04-13 18:48:48 -0400

    Simplify building

--- aosp_kernel.sh
+++ aosp_kernel.sh
@@ -4,22 +4,34 @@
 WORK=`pwd`
 DATE=$(date +%m%d)
 
+# execution!
+cd ..
+
 # check for voodoo/non-voodoo
 if [ "$1" != "V" ]; then
 	CONFIG="novoodoo"
+	cd fascinate_initramfs
+	git checkout aosp
+	mv .git ../
 else
 	CONFIG="voodoo"
+	cd fascinate_initramfs
+	git checkout aosp-voodoo
+	mv .git ../
 fi
 
 # build the kernel
+cd $WORK
 echo "***** Building : AOSP $CONFIG *****"
 make clean mrproper > /dev/null 2>&1
 rm -f update_"$CONFIG"/*.zip update_"$CONFIG"/kernel_update/zImage
 
-make ARCH=arm aosp_"$CONFIG"_defconfig 1>/dev/null 2>"$WORK"/errlog.txt
-make -j6 CROSS_COMPILE=/opt/toolchains/arm-2009q3/bin/arm-none-linux-gnueabi- ARCH=arm HOSTCFLAGS="-g -O3" 1>"$WORK"/stdlog.txt 2>>"$WORK"/errlog.txt
+make ARCH=arm aosp_defconfig 1>/dev/null 2>"$WORK"/errlog.txt
+make -j6 CROSS_COMPILE=/opt/toolchains/arm-2010q1/bin/arm-none-linux-gnueabi- ARCH=arm HOSTCFLAGS="-g -O3" 1>"$WORK"/stdlog.txt 2>>"$WORK"/errlog.txt
 if [ $? != 0 ]; then
 		echo -e "FAIL!\n"
+		cd ..
+		mv .git fascinate_initramfs/
 		exit 1
 	else
 		echo -e "Success!\n"
@@ -30,7 +42,9 @@ cp arch/arm/boot/zImage update_"$CONFIG"/kernel_update/zImage
 cd update_"$CONFIG"
 zip -r -q kernel_update.zip .
 mv kernel_update.zip ../"$DATE"_aosp_"$CONFIG".zip
-cd ..
+cd ../../
+mv .git fascinate_initramfs/
+cd $WORK
 echo -e "***** Successfully compiled: AOSP $CONFIG *****\n"
 
 if [ "$1" == "A" ]; then ./aosp_kernel.sh V; fi
--- build_kernel.sh
+++ build_kernel.sh
@@ -10,8 +10,14 @@ cd ..
 # check for voodoo/non-voodoo
 if [ "$1" != "V" ]; then
 	CONFIG="novoodoo"
+	cd fascinate_initramfs
+	git checkout froyo
+	mv .git ../
 else
 	CONFIG="voodoo"
+	cd fascinate_initramfs
+	git checkout froyo-voodoo
+	mv .git ../
 fi
 
 # build the kernel
@@ -20,10 +26,12 @@ echo "***** Building : $CONFIG *****"
 make clean mrproper > /dev/null 2>&1
 rm -f update_"$CONFIG"/*.zip update_"$CONFIG"/kernel_update/zImage
 
-make ARCH=arm imnuts_"$CONFIG"_defconfig 1>/dev/null 2>"$WORK"/errlog.txt
-make -j6 CROSS_COMPILE=/opt/toolchains/arm-2009q3/bin/arm-none-linux-gnueabi- ARCH=arm HOSTCFLAGS="-g -O3" 1>"$WORK"/stdlog.txt 2>>"$WORK"/errlog.txt
+make ARCH=arm imnuts_defconfig 1>/dev/null 2>"$WORK"/errlog.txt
+make -j6 CROSS_COMPILE=/opt/toolchains/arm-2010q1/bin/arm-none-linux-gnueabi- ARCH=arm HOSTCFLAGS="-g -O3" 1>"$WORK"/stdlog.txt 2>>"$WORK"/errlog.txt
 if [ $? != 0 ]; then
 		echo -e "FAIL!\n"
+		cd ..
+		mv .git fascinate_initramfs/
 		exit 1
 	else
 		echo -e "Success!\n"
@@ -34,7 +42,9 @@ cp arch/arm/boot/zImage update_"$CONFIG"/kernel_update/zImage
 cd update_"$CONFIG"
 zip -r -q kernel_update.zip .
 mv kernel_update.zip ../"$DATE"_imnuts_"$CONFIG".zip
-cd ..
+cd ../../
+mv .git fascinate_initramfs/
+cd $WORK
 echo -e "***** Successfully compiled: $CONFIG *****\n"
 
 if [ "$1" == "A" ]; then ./build_kernel.sh V; fi
